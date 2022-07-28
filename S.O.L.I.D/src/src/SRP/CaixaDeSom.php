b8c7e2faefad3924782430d1179810b5563eea48
Rubia <rubiaemilly29@gmail.com>
2022-07-28 13:36:14 -0300

    iniciando hand on S.O.L.I.D

---
+++ S.O.L.I.D/src/src/SRP/CaixaDeSom.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Rubia\SOLID\src\SRP;
+
+class CaixaDeSom
+{
+    private $volume;
+    private $ligado;
+    private $emitirSom;
+
+
+}
---
+++ S.O.L.I.D/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a0542af5dc7c76edfeb919d362fc39069822a6ef',
+        'name' => 'rubia/s.o.l.i.d',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rubia/s.o.l.i.d' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a0542af5dc7c76edfeb919d362fc39069822a6ef',
+            'dev_requirement' => false,
+        ),
+    ),
+);
